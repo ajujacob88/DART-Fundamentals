203fbe6294179fff8cef3e38b978599b3275f59c
ajujacob <ajujacob88@gmail.com>
2023-12-11 19:59:08 +0530

    added pgms

---
+++ dart_basics/basics_dart/bin/basics_dart.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+void main() {
+  /*  //SECTION 1
+  print('Hello world: !');
+  var a = stdin.readLineSync();
+  print('hello $a');
+  stdout.write('Another print $a \n');
+
+  //variables
+  //strings
+  var name = "Aju";
+  String myName = "Aju";
+
+  //integer
+  var num1 = 20;
+  int num2 = 25;
+
+  print(name + myName + num1.toString() + num2.toString());
+
+  print('$name $myName $num1 $num2');
+
+  //Dynamic can be used instead of var.. generally stick with var keyword
+  dynamic name1 = "Rahul";
+  print(name1);
+
+  //declaring creates nul value
+  var aju;
+  print(aju);
+
+  aju = "This is my name";
+  print(aju);
+
+  */
+
+  //SECTION 2
+  /*
+  print("Enter 2 numbers");
+  var input1 = stdin.readLineSync();
+  var input2 = stdin.readLineSync();
+  var sum = int.parse(input1!) +
+      int.parse(
+          input2!); //convert string to int | ! is to make sure null is input is not null
+  print(sum);
+
+  //if we want to write without ! - Exception handling/null check
+  var input3 = stdin.readLineSync();
+  // if (input3 == null) {
+  //   input3 = "0";
+  // }
+
+  // or
+
+  input3 ??= "0"; //null check using ?? operator instead of if
+
+  var num3 = int.parse(input3);
+  print(num3 + num3);
+
+  print("product = ${num3 * num3}");
+
+  */
+
+  //SECTION 3 - DATA TYPES
+}
